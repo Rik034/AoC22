cfe9d7fab5cb3630f1ffcd8f8c063ff3abc8a81c
Rik034 <riccardo.riccardoroman@gmail.com>
2022-12-04 03:38:54 +0100

    moved map to global scope

--- day3/main.go
+++ day3/main.go
@@ -6,6 +6,62 @@ import (
 	"os"
 )
 
+var priorities = map[string]int{
+	"a": 1,
+	"b": 2,
+	"c": 3,
+	"d": 4,
+	"e": 5,
+	"f": 6,
+	"g": 7,
+	"h": 8,
+	"i": 9,
+	"j": 10,
+	"k": 11,
+	"l": 12,
+	"m": 13,
+	"n": 14,
+	"o": 15,
+	"p": 16,
+	"q": 17,
+	"r": 18,
+	"s": 19,
+	"t": 20,
+	"u": 21,
+	"v": 22,
+	"w": 23,
+	"x": 24,
+	"y": 25,
+	"z": 26,
+
+	"A": 27,
+	"B": 28,
+	"C": 29,
+	"D": 30,
+	"E": 31,
+	"F": 32,
+	"G": 33,
+	"H": 34,
+	"I": 35,
+	"J": 36,
+	"K": 37,
+	"L": 38,
+	"M": 39,
+	"N": 40,
+	"O": 41,
+	"P": 42,
+	"Q": 43,
+	"R": 44,
+	"S": 45,
+	"T": 46,
+	"U": 47,
+	"V": 48,
+	"W": 49,
+	"X": 50,
+	"Y": 51,
+	"Z": 52,
+}
+
 func ReadByLine(filename string) []string {
 	lines := []string{}
 	file, err := os.Open(filename)
@@ -23,62 +79,6 @@ func ReadByLine(filename string) []string {
 }
 
 func part1(input []string) int {
-	priorities := map[string]int{
-		"a": 1,
-		"b": 2,
-		"c": 3,
-		"d": 4,
-		"e": 5,
-		"f": 6,
-		"g": 7,
-		"h": 8,
-		"i": 9,
-		"j": 10,
-		"k": 11,
-		"l": 12,
-		"m": 13,
-		"n": 14,
-		"o": 15,
-		"p": 16,
-		"q": 17,
-		"r": 18,
-		"s": 19,
-		"t": 20,
-		"u": 21,
-		"v": 22,
-		"w": 23,
-		"x": 24,
-		"y": 25,
-		"z": 26,
-
-		"A": 27,
-		"B": 28,
-		"C": 29,
-		"D": 30,
-		"E": 31,
-		"F": 32,
-		"G": 33,
-		"H": 34,
-		"I": 35,
-		"J": 36,
-		"K": 37,
-		"L": 38,
-		"M": 39,
-		"N": 40,
-		"O": 41,
-		"P": 42,
-		"Q": 43,
-		"R": 44,
-		"S": 45,
-		"T": 46,
-		"U": 47,
-		"V": 48,
-		"W": 49,
-		"X": 50,
-		"Y": 51,
-		"Z": 52,
-	}
-
 	found := false
 	sum := 0
 
@@ -101,8 +101,8 @@ func part1(input []string) int {
 }
 
 func main() {
-	input := ReadByLine("sample.txt")
-	// input := ReadByLine("input.txt")
-	priorities := part1(input)
-	fmt.Println("Part 1 = ", priorities)
+	// input := ReadByLine("sample.txt")
+	input := ReadByLine("input.txt")
+	sumOfPriorities := part1(input)
+	fmt.Println("Part 1 = ", sumOfPriorities)
 }
